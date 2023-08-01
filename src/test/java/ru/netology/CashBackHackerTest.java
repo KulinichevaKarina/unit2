9b5f1bb1fc42d039f00fd63dc9e7cfbbf3aad172
Karina <kulinicheva_91@mail.ru>
2023-08-01 23:17:49 +0300

    CashBackHacker with junit4

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -1,41 +1,43 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashBackHackerTest {
     CashBackHacker cash = new CashBackHacker();
+
     @Test
     public void ShouldCashTestMin() {
         int amount = 900;
 
-        int actual = cash.remain(900);
         int expected = 100;
+        int actual = cash.remain(900);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+
     }
 
     @Test
     public void ShouldCashTestNormal() {
         int amount = 1000;
 
-        int actual = cash.remain(1000);
         int expected = 0;
+        int actual = cash.remain(1000);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void ShouldCashTestMax() {
         int amount = 1100;
 
-        int actual = cash.remain(1100);
         int expected = 900;
+        int actual = cash.remain(1100);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
-
 
 }
 
